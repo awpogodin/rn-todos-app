170606baa83fa68705fa1649e6dbdfce2146d934
Alexandr <awpogodin@mail.ru>
2020-02-21 22:49:47 +0400

    Add AsyncStorage

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, {useEffect} from 'react';
-import { StyleSheet, View } from 'react-native';
+import { AsyncStorage, StyleSheet, View } from 'react-native';
 import { AppLoading } from 'expo';
 import * as Font from 'expo-font';
 import { Ionicons } from '@expo/vector-icons';
@@ -7,14 +7,16 @@ import NavBar from "./src/components/NavBar";
 import TodosList from "./src/components/TodosList";
 import AddTodo from "./src/components/AddTodo";
 
-const initTodos = [
-  {id: '1', title: 'Купить хлеб', completed: false},
-  {id: '2', title: 'Купить молоко', completed: true},
-  {id: '3', title: 'Купить масло', completed: false},
-];
+const STORAGE = 'Todos';
+
+// const initTodos = [
+//   {id: '1', title: 'Купить хлеб', completed: false},
+//   {id: '2', title: 'Купить молоко', completed: true},
+//   {id: '3', title: 'Купить масло', completed: false},
+// ];
 
 export default function App() {
-  const [todos, setTodos] = React.useState([...initTodos]);
+  const [todos, setTodos] = React.useState([]);
   const [isReady, setIsReady] = React.useState(false);
 
   useEffect(() => {
@@ -24,11 +26,39 @@ export default function App() {
         Roboto_medium: require('native-base/Fonts/Roboto_medium.ttf'),
         ...Ionicons.font,
       });
+      await _retrieveData();
       setIsReady(true);
     };
 
     loadAsync();
   }, []);
+
+  useEffect(() => {
+    const asyncStoreData = async () => {
+      await _storeData();
+    };
+
+    asyncStoreData();
+  }, [todos]);
+
+  const _storeData = async () => {
+    try {
+      await AsyncStorage.setItem(STORAGE, JSON.stringify(todos));
+    } catch (error) {
+      // Error saving data
+    }
+  };
+
+  const _retrieveData = async () => {
+    try {
+      const value = await AsyncStorage.getItem(STORAGE);
+      if (value !== null) {
+        setTodos(JSON.parse(value));
+      }
+    } catch (error) {
+      // Error retrieving data
+    }
+  };
 
   const addTodo = (title) => {
     if (title.trim()) {
